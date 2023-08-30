808b4ba703ca55c46f9f57321aecbd4c759462a5
Cipher <cipher@SAN-Kali1.www.github.com//cipher58>
2023-08-30 16:47:14 +0100

    Added vector resizing

--- AI/janex_utils.py
+++ AI/janex_utils.py
@@ -1,11 +1,30 @@
 from Janex import *
+import numpy as np
+
+progressive_verbs = {
+    "go": "Going",
+    "open": "Opening"
+}
 
 def word_tokenize(input_string):
     words = tokenize(input_string)
     return words
 
-def cosine_similarity(one, two):
-    similarity = calculate_cosine_similarity(one, two)
+def cosine_similarity(vector1, vector2):
+    # Resize vectors to a common dimension
+    target_dim = 300
+    vector1 = np.resize(vector1, target_dim)
+    vector2 = np.resize(vector2, target_dim)
+
+    # Calculate cosine similarity between two vectors
+    dot_product = np.dot(vector1, vector2)
+    norm_vector1 = np.linalg.norm(vector1)
+    norm_vector2 = np.linalg.norm(vector2)
+
+    if norm_vector1 == 0 or norm_vector2 == 0:
+        return 0  # Handle zero division case
+
+    similarity = dot_product / (norm_vector1 * norm_vector2)
     return similarity
 
 def token_compare(sentence1, sentence2):
@@ -39,3 +58,35 @@ def compare_lists(list1, list2):
     percentage = (common / totallength) * 100
 
     return percentage
+
+def alter_sentence(string):
+    stationary = ["open", "start", "run", "begin", "launch", "go"]
+    new_string = []
+
+    words = tokenize(string)
+    print(words)
+
+    for word in words:
+        if word in stationary:
+            new_word = f"{word}ing"
+            if word.istitle():
+                new_word = new_word.capitalize()
+            new_string.append(f"{new_word}")
+        else:
+            new_word = word
+            new_string.append(f"{new_word}")
+
+    new_sentence = " ".join(new_string)
+    new_sentence = f"{new_sentence.capitalize()}."
+    return new_sentence
+
+def vectorize(input_string):
+    dimensions = len(input_string)
+    vector = np.zeros(dimensions)
+
+    for i, char in enumerate(input_string):
+        if i >= dimensions:
+            break
+        vector[i] = ord(char)  # Get the ASCII value of the character
+
+    return vector
--- AI/toolkit.py
+++ AI/toolkit.py
@@ -12,16 +12,23 @@ class chatbot:
         return data
 
     def say(self, input_string):
+        most_similar_class = None
         meanings, sentence_meanings, highest_similarity = [], [], 0
         tokens = tokenize(input_string)
         for token in tokens:
             meaning = get_meaning(token)
+            if not meaning:
+                meaning = vectorize(token)
+                meaning = f"{meaning}"
             meanings.append(meaning)
         for intent_class in self.intents["intents"]:
             for sentence in intent_class["sentences"]:
                 sentence_tokens = tokenize(sentence)
                 for token in sentence_tokens:
                     meaning = get_meaning(token)
+                    if not meaning:
+                        meaning = vectorize(token)
+                        meaning = f"{meaning}"
                     sentence_meanings.append(meaning)
 
             similarity = compare_lists(meanings, sentence_meanings)
@@ -31,9 +38,24 @@ class chatbot:
                 highest_similarity = similarity
                 self.most_similar_class = most_similar_class
 
-        if most_similar_class:
+        if most_similar_class is not None:
             responses = most_similar_class['sentences']
             response = random.choice(responses)
+            response = alter_sentence(response)
+            return response
+        else:
+            for intent_class in self.intents["intents"]:
+                sentence = random.choice(intent_class["sentences"])
+                sentence_vectors = vectorize(sentence)
+                input_vectors = vectorize(input_string)
+                similarity = cosine_similarity(sentence_vectors, input_vectors)
+                if similarity > highest_similarity:
+                    highest_similarity = similarity
+                    most_similar_class = intent_class
+                    self.most_similar_class = most_similar_class
+            responses = most_similar_class['sentences']
+            response = random.choice(responses)
+            response = alter_sentence(response)
             return response
 
     def get_class(self):
