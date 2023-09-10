51b0f7104b75af70bbf4a674cb267303173a2f18
Cipher58 <jackfranklin899@gmail.com>
2023-09-10 15:52:33 +0100

    GG MF

--- AI/main.py
+++ AI/main.py
@@ -7,6 +7,7 @@ class JarvisAI:
     def __init__(self):
         self.chatbot = JanexBot("AI/database.json", "en_core_web_sm")
         self.classifier = JanexPT("AI/intents.json")
+        self.classifier.modify_data_path("BinaryFiles/data.pth")
         self.previous_input = None
         self.response_check()
 
