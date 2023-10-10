29151abc152b274853b66717c5019ba7d6c04003
Cipher58 <jackfranklin899@gmail.com>
2023-10-10 06:12:22 +0100

    Reverted to JB

--- AI/main.py
+++ AI/main.py
@@ -1,18 +1,19 @@
 from gpt4all import GPT4All
 from Janex import *
 from JanexPT import *
+from JanexBot import *
 from Utilities.functions import *
 
 settings = loadconfig("./Settings/configuration.json")
 
 class JarvisAI:
     def __init__(self):
+        self.chatbot = JanexBot("AI/database.json", "en_core_web_sm")
         self.classifier = JanexPT("AI/intents.json")
         self.classifier.modify_data_path("BinaryFiles/data.pth")
         self.previous_input = None
         self.response_check()
         self.classifier.device = torch.device('cpu')
-        self.gpt = GPT4All("orca-mini-3b.ggmlv3.q4_0.bin")
 
     def say(self, input_string):
         self.response_check()
@@ -20,14 +21,9 @@ class JarvisAI:
         self.previous_input = input_string
         if self.response_setting == "random":
             intent_class = self.get_class()
-            context = intent_class.get("context")
-            if context == "command":
-                responses = intent_class["responses"]
-                response = random.choice(responses)
-                return response
-            else:
-                Response = self.gpt.generate(f'{settings.get("SystemPrompt")}\n ###User: {input_string} \n %1 \n ### Response: ', max_tokens=64)
-                return Response
+            responses = intent_class["responses"]
+            response = random.choice(responses)
+            return response
         else:
             intent_class = self.get_class()
             if intent_class:
